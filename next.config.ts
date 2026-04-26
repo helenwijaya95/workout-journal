4b58a8642e660d1bdecf9821ede792cf1ee679b1
helenwijaya95 <helen.wijaya95@gmail.com>
2026-04-27 01:37:21 +0700

    Update config to debug middleware error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false, // disable for debugging only
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
